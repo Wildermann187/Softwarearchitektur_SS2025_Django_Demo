fe929298bf913c1ea848050017286c554236bd2a
Wildermann187 <slim.laifi@stud.hn.de>
2025-06-26 06:17:34 +0200

    readme

--- hello/admin.py
+++ hello/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from .models import LogMessage
-from .models import Author
-from myproject.myapp.models import Author
-
-class AuthorAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Author, AuthorAdmin)
 
 # Register your models here.
 admin.site.register(LogMessage)
